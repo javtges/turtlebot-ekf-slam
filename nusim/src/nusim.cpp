44eb23abfad2b9cd1ad6cded1633ee3fde74411f
James Avtges <jamesavtges2022@u.northwestern.edu>
2022-01-21 17:01:21 -0600

    markers show upgit statusgit status!

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <string>
 #include <std_msgs/UInt64.h>
+#include <ros/console.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_srvs/Empty.h>
@@ -16,10 +17,13 @@
 
 static std_msgs::UInt64 ts;
 static sensor_msgs::JointState jointState;
-// static tf2_ros::TransformBroadcaster br;
 static geometry_msgs::TransformStamped transformStamped;
+static visualization_msgs::MarkerArray ma;
 static double x, y, theta;
 static double x_0, y_0, theta_0;
+std::vector<double> radii;
+std::vector<double> x_locs;
+std::vector<double> y_locs;
 
 bool resetCallback(std_srvs::Empty::Request &Request, std_srvs::Empty::Response &Response){
     ts.data = 0;
@@ -37,6 +41,44 @@ bool teleportCallback(nusim::teleport::Request &Request, nusim::teleport::Respon
 }
 
 
+visualization_msgs::MarkerArray addObstacles(std::vector<double> radii, std::vector<double> x_locs, std::vector<double> y_locs){
+
+    int l = 3; //radii.size();
+    visualization_msgs::MarkerArray maTemp;
+    maTemp.markers.resize(l);
+
+    for (int i=0; i<l; i++){
+        
+        maTemp.markers[i].header.frame_id = "world";
+        maTemp.markers[i].header.stamp = ros::Time::now();
+        maTemp.markers[i].ns = "obstacles";
+        maTemp.markers[i].id = i;
+        maTemp.markers[i].type = visualization_msgs::Marker::CYLINDER;
+        maTemp.markers[i].action = visualization_msgs::Marker::ADD;
+
+        maTemp.markers[i].pose.position.x = x_locs[i];
+        maTemp.markers[i].pose.position.z = 0.125;
+        maTemp.markers[i].pose.position.y = y_locs[i];
+        maTemp.markers[i].pose.orientation.x = 0.0;
+        maTemp.markers[i].pose.orientation.y = 0.0;
+        maTemp.markers[i].pose.orientation.z = 0.0;
+        maTemp.markers[i].pose.orientation.w = 1.0;
+
+        maTemp.markers[i].scale.x = radii[i]*2;
+        maTemp.markers[i].scale.y = radii[i]*2;
+        maTemp.markers[i].scale.z = 0.25;
+
+        maTemp.markers[i].color.r = 1.0;
+        maTemp.markers[i].color.g = 0.0;
+        maTemp.markers[i].color.b = 0.0;
+        maTemp.markers[i].color.a = 1.0;
+        maTemp.markers[i].lifetime = ros::Duration(0);
+    }
+    return maTemp;
+
+}
+
+
 int main(int argc, char * argv[])
 {
     ros::init(argc, argv, "nusim");
@@ -45,29 +87,30 @@ int main(int argc, char * argv[])
     ros::NodeHandle n;
 
     // read parameters, create publishers/subscribers
-    int frequency = 500;
-    nh.setParam("frequency", 500);
-    // nh.setParam("x0", 0);
-    // nh.setParam("y0", 0);
-    // nh.setParam("theta0", 0);
+    int frequency;
+    nh.param("frequency",frequency, 500);
     nh.param("x0", x_0, 0.0);
     nh.param("y0", y_0, 0.0);
     nh.param("theta0", theta_0, 0.0);
 
-    int fq;
-    nh.getParam("frequency", fq);
+    nh.getParam("radii", radii);
+    nh.getParam("x_pos", x_locs);
+    nh.getParam("y_pos", y_locs);
 
     ros::Rate r(frequency);
 
-    ros::Publisher ts_pub = nh.advertise<std_msgs::UInt64>("timestep", fq);
-    ros::Publisher joint_state_pub = n.advertise<sensor_msgs::JointState>("joint_states",1);
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::MarkerArray>("obstacles",0);
+    ros::Publisher ts_pub = nh.advertise<std_msgs::UInt64>("timestep", frequency);
+    ros::Publisher joint_state_pub = n.advertise<sensor_msgs::JointState>("joint_states",10);
+    ros::Publisher marker_pub = nh.advertise<visualization_msgs::MarkerArray>("obstacles",10, true);
 
     ros::ServiceServer resetService = nh.advertiseService("reset", resetCallback);
     ros::ServiceServer advertiseService = nh.advertiseService("teleport", teleportCallback);
 
     x = x_0; y = y_0; theta = theta_0;
     ts.data = 0;
+
+    ma = addObstacles(radii, x_locs, y_locs);
+    marker_pub.publish(ma);
 
 
     while(ros::ok())
