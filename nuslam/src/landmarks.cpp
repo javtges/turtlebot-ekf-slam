e55d883aee58e34036ef2992b589aa21a8687d2b
James Avtges <jamesavtges2022@u.northwestern.edu>
2022-03-14 14:35:27 -0500

    clustering works properly

--- nuslam/src/landmarks.cpp
+++ nuslam/src/landmarks.cpp
@@ -27,9 +27,94 @@
 
 static int frequency;
 static int init_flag = 1;
+// static std::vector<std::vector<float>> clusters;
 
 void laser_scan_callback(const sensor_msgs::LaserScan & msg){
     ROS_WARN_STREAM("AAA");
+
+    int length = msg.ranges.size();
+    double threshold = 0.2;
+    float range = 0.0, prev_range = 0.0, prev_distance = 0.0, distance = 0.0;
+    std::vector<std::vector<turtlelib::Vector2D>> clusters;
+    int n_clusters = 0;
+
+    for(int i=0; i<length; i++){
+        
+        // Check the value at the angle and the value behind it
+        // If they're similar, add to cluster
+        // If not, make a new cluster
+
+        range = msg.ranges[i];
+        
+        if(i==0){
+            prev_range = msg.ranges[length-1];
+            double mx = std::cos(turtlelib::deg2rad(i)) * (prev_range);
+            double my = std::sin(turtlelib::deg2rad(i)) * (prev_range);
+
+            prev_distance = std::sqrt( std::pow(mx,2) + std::pow(my,2));
+        }
+        else{
+            prev_range = msg.ranges[i-1];
+            double mx = std::cos(turtlelib::deg2rad(i)) * (prev_range);
+            double my = std::sin(turtlelib::deg2rad(i)) * (prev_range);
+
+            prev_distance = std::sqrt( std::pow(mx,2) + std::pow(my,2));
+        }
+
+        double mx = std::cos(turtlelib::deg2rad(i)) * (range);
+        double my = std::sin(turtlelib::deg2rad(i)) * (range);
+
+        distance = std::sqrt( std::pow(mx,2) + std::pow(my,2));
+
+        double dist_diff = std::abs(distance - prev_distance);
+
+        
+        if (range > 0.0){
+            ROS_WARN_STREAM(i << " " << range << " " << mx << " " << my << " distance " << distance);
+            
+            if(n_clusters == 0){  // First cluster
+                    std::vector<turtlelib::Vector2D> new_cluster;
+                    new_cluster.push_back ({mx, my});
+                    clusters.push_back (new_cluster);
+                    n_clusters++;
+            }
+            else{ // Already existing clusters
+                if (dist_diff < threshold){ // If similar to previous
+                    // Add to cluster
+                    clusters.at(n_clusters-1).push_back ({mx, my});
+                }
+                else{ // Make a new cluster
+                    std::vector<turtlelib::Vector2D> new_cluster;
+                    new_cluster.push_back ({mx, my});
+                    clusters.push_back (new_cluster);
+                    ROS_ERROR_STREAM("new cluster!");
+                    n_clusters++;
+                }
+            }
+        }
+
+        // Compare first and last. If close, merge. Otherwise, don't
+        
+        
+    }    
+    turtlelib::Vector2D first = clusters.at(0).at(0);
+
+    std::vector<turtlelib::Vector2D> first_cluster = clusters.at(0);
+    std::vector<turtlelib::Vector2D> last_cluster = clusters.at(n_clusters-1);
+
+    ROS_WARN_STREAM( first_cluster.size() << " " << last_cluster.size());
+
+    double first_distance = std::sqrt( std::pow(first.x,2) + std::pow(first.y,2) );
+    if (std::abs(first_distance - distance) < threshold){
+        clusters.at(0).insert( clusters.at(0).end(), clusters.at(n_clusters-1).begin(), clusters.at(n_clusters-1).end() );
+        clusters.pop_back(); // Removes last cluster
+        ROS_WARN_STREAM("overlap clusters, " << clusters.at(0).size());
+        
+    }
+    
+    ROS_WARN_STREAM(clusters.size());
+
+
 }
 
 
